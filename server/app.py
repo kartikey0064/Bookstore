0eacdd41275e367da1a1e85797224e3e48c9c5a2
kartikey0064 <kartikey0064@users.noreply.github.com>
2026-03-31 17:23:43 +0530

    Fix requirements.txt and deployment

--- server/app.py
+++ server/app.py
@@ -8,5 +8,5 @@ app = create_app()
 ping_db()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "5000"))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
